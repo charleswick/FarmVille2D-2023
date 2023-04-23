215a6c40dbebcaf00ac731f2527685d407cec26e
charleswick <charles_wick25@milton.edu>
2023-04-23 10:46:44 -0400

    made the print plot method and randomized the plants

--- src/MyFarm.java
+++ src/MyFarm.java
@@ -1,8 +1,14 @@
 public class MyFarm {
     public static void main(String[] args) {
         MyFarm charlie = new MyFarm();
+        Plot Ryan = new Plot();
+        Ryan.printPlot();
+
+
     }
     public MyFarm(){
         System.out.println("Hello from my farm!");
+
     }
+
 }
--- src/Plot.java
+++ src/Plot.java
@@ -14,7 +14,19 @@ public class Plot {
         if (num ==1){
             plantName = "corn";
         }
+        if (num ==2){
+            plantName = "carrots";
+        }
+        if (num ==3){
+            plantName = "tomato";
+        }
+        if (num ==4){
+            plantName = "empty";
+        }
 
+    }
+    public void printPlot(){
+        System.out.println("There is a "+plantName+" plant in the plot that has "+numberOfplants + " plants and is "+needsWater +" that it needs water.");
     }
 
 }
