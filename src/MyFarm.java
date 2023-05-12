06553c1856377a5fffb70381aa47df5cc29eed86
charleswick <charles_wick25@milton.edu>
2023-05-12 12:21:02 -0400

    in class

--- src/MyFarm.java
+++ src/MyFarm.java
@@ -1,11 +1,6 @@
 public class MyFarm {
 
-    public static void main(String[] args) {
-        MyFarm charlie = new MyFarm();
 
-
-
-    }
     public Plot[][] grid;
     public MyFarm(){
         System.out.println("Hello from my farm!");
---
+++ src/RunMyFarm.java
@@ -0,0 +1,8 @@
+public class RunMyFarm {
+    public static void main(String[] args) {
+        MyFarm charlie = new MyFarm();
+
+
+
+    }
+}
